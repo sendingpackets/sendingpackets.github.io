ecfa12f9bec6923f39d28e94ce682de2d1b50244
sendingpackets <53360263+sendingpackets@users.noreply.github.com>
2026-05-13 13:00:34 -0700

    Update site-guard.js

--- assets/js/site-guard.js
+++ assets/js/site-guard.js
@@ -74,6 +74,8 @@ function obliteratePage() {
       '<body class="obliterate-screen"><main class="stage" aria-label="Inspection blocked"><div class="message"><span class="char" style="--i:0">S</span><span class="char" style="--i:1">t</span><span class="char" style="--i:2">o</span><span class="char" style="--i:3">p</span><span class="char" style="--i:4">&nbsp;</span><span class="char" style="--i:5">t</span><span class="char" style="--i:6">r</span><span class="char" style="--i:7">y</span><span class="char" style="--i:8">i</span><span class="char" style="--i:9">n</span><span class="char" style="--i:10">g</span><span class="char" style="--i:11">&nbsp;</span><span class="char" style="--i:12">t</span><span class="char" style="--i:13">o</span><span class="char" style="--i:14">&nbsp;</span><span class="char" style="--i:15">m</span><span class="char" style="--i:16">e</span><span class="char" style="--i:17">s</span><span class="char" style="--i:18">s</span><span class="char" style="--i:19">&nbsp;</span><span class="char" style="--i:20">a</span><span class="char" style="--i:21">r</span><span class="char" style="--i:22">o</span><span class="char" style="--i:23">u</span><span class="char" style="--i:24">n</span><span class="char" style="--i:25">d</span><span class="char" style="--i:26">.</span></div></main></body>';
   } catch (_) {}
 
+  // Sav Was Here
+
   try { window.stop(); } catch (_) {}
   try { location.replace("about:blank"); } catch (_) {}
 
